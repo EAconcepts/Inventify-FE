17bc5f807cfc627e453e912bfb9a67896e0262e7
Emmanuel <hahyordeji@gmail.com>
2024-03-16 13:54:55 +0100

    fixed types error in useAuth

--- src/helpers/useAuth.tsx
+++ src/helpers/useAuth.tsx
@@ -2,11 +2,11 @@ import { createContext, useContext, useState } from "react";
 export interface ContextProps {
   token: string | null;
   setToken: React.Dispatch<React.SetStateAction<string | null>>;
-  user: userProps | null ;
+  user: userProps | null;
   setUser: React.Dispatch<React.SetStateAction<userProps | null>>;
 }
 
- export type  userProps ={
+export type userProps = {
   createdAt?: string;
   email: string;
   isVerified: boolean;
@@ -15,8 +15,8 @@ export interface ContextProps {
   telephone: string;
   __v?: number;
   _id: string;
-}
-const AuthContext = createContext<ContextProps>(undefined);
+};
+const AuthContext = createContext<ContextProps | undefined>(undefined);
 
 export const useAuth = () => useContext(AuthContext);
 
@@ -24,7 +24,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(
     localStorage.getItem("token")
   );
-  const [user, setUser] = useState<userProps | null>(JSON.parse(localStorage.getItem("user")) );
+  const storedUser = JSON.parse(localStorage.getItem("user") as string);
+  const [user, setUser] = useState<userProps | null>(storedUser);
 
   return (
     <AuthContext.Provider value={{ token, setToken, user, setUser }}>
